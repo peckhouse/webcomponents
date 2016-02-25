299525c7faf1eb75e3cbebf16dd18185699c40b6
peckhouse <millennionprandi@gmail.com>
2016-02-25 14:45:15 +0900

    implement the global grid system.

---
+++ src/js/components/modules/module-peck-tooltip.js
@@ -0,0 +1,13 @@
+//============================================================
+//
+// Module JS: peck-tooltip
+//
+// @description
+//   Javacript for a tooltip following:
+//     -The SMACSS architecture (https://smacss.com/) 
+// @version 0.0.1
+//
+// @author L.Prandi
+//
+//============================================================
+
